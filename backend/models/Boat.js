b8fa75ecaccec4d4d4d7206c3d59e2bcbdbff11a
Workshop Participant <jegatheeswaran@gmx.ch>
2024-12-08 18:24:12 +0100

    add additional  Boat attributes

--- backend/models/Boat.js
+++ backend/models/Boat.js
@@ -4,8 +4,12 @@ mongoose.connect(config.mongoURI)
 
 const boatSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
-});
+  description: { type: String, required: false },
+  year: { type: String, required: true },
+  location: { type: String, required: true },
+  value: { type: Number, required: true },
+  }
+);
 
 const Boat= mongoose.model('BoatList', boatSchema);
 module.exports = Boat;
