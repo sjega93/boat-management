a90144758293e4028890429d17a73a253206b141
Workshop Participant <jegatheeswaran@gmx.ch>
2024-12-08 18:26:19 +0100

    add DetailView

---
+++ src/components/DetailViewComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <h1>Boat Details</h1>
+        <div v-if="boat">
+            <p><strong>Name:</strong> {{ boat.name }}</p>
+            <p><strong>Year:</strong> {{ boat.year }}</p>
+            <p><strong>Location:</strong> {{ boat.location }}</p>
+            <p><strong>Value:</strong> {{ boat.value }}</p>
+            <p><strong>Further Description</strong> {{ boat.description }}</p>
+
+        </div>
+        <div v-else>
+            <p>Loading boat details...</p>
+        </div>
+    </div>
+    </template>
+    <script>
+    import axios from 'axios';
+    
+    export default {
+        data() {
+            return {
+                boat: null
+            };
+        },
+        created() {
+            let id = this.$route.params.id;
+            let apiURL = `http://localhost:4000/api/${id}`;
+            axios.get(apiURL).then(res => {
+                this.boat = res.data;
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+    };
+    </script>
+    <style lang="">
+    </style>
+    
