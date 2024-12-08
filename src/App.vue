26140d84910395bae2b30c18d0670d6ec10d3a3b
Workshop Participant <jegatheeswaran@gmx.ch>
2024-12-08 15:06:22 +0100

    add sign out button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <script setup>
+  import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+  import { Amplify } from 'aws-amplify';
+  import outputs from '../backend/amplify_outputs.json'
+  Amplify.configure(outputs);
 </script>
 
 <template>
-    <div>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <div>
         <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
-            <div class="container">
-                <router-link :to="{ path: '/' }" class="navbar-brand float-left">CRUD Project</router-link>
+          <div class="container">
+            <router-link :to="{ path: '/' }" class="navbar-brand float-left">CRUD Project</router-link>
             <ul class="nav navbar-nav flex-row float-right">
-                <li class="nav-item">
-                    <router-link to="/create" class="nav-link pr-3">Create Boat</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/view" class="nav-link pr-3">View Boats</router-link>
-                </li>
+              <li class="nav-item">
+                <router-link to="/create" class="nav-link pr-3">Create Boat</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/view" class="nav-link pr-3">View Boats</router-link>
+              </li>
+              <li class="nav-item">
+                <button @click="signOut" class="nav-link pr-3 btn btn-link">Sign Out</button>
+              </li>
             </ul>
-            </div>
+          </div>
         </nav>
 
         <div class="container mt-5">
-            <router-view></router-view>
+          <router-view></router-view>
         </div>
-    </div>
+      </div>
+    </template>
+  </authenticator>
 </template>
-
-<style scoped>
-
-</style>
