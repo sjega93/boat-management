2ebc91c85eec7378cff35e5d654009f195dab5b6
Workshop Participant <jegatheeswaran@gmx.ch>
2024-12-10 02:36:01 +0100

    validation frontend inputs from user

--- src/components/CreateComponent.vue
+++ src/components/CreateComponent.vue
@@ -5,26 +5,63 @@
             <form @submit.prevent="handleSubmitForm">
                 <div class="form-group mb-3">
                     <label for="name">Name</label>
-                    <input type="text" class="form-control" v-model="Boat.name" required>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="Boat.name"
+                        @blur="validateField('name')"
+                        :class="{'is-invalid': errors.name}"
+                    >
+                    <div v-if="errors.name" class="invalid-feedback">{{ errors.name }}</div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="Year">Year</label>
-                    <input type="text" class="form-control" v-model="Boat.year" required>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="Boat.year"
+                        @blur="validateField('year')"
+                        :class="{'is-invalid': errors.year}"
+                    >
+                    <div v-if="errors.year" class="invalid-feedback">{{ errors.year }}</div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="Location">Location</label>
-                    <input type="text" class="form-control" v-model="Boat.location" required>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="Boat.location"
+                        @blur="validateField('location')"
+                        :class="{'is-invalid': errors.location}"
+                    >
+                    <div v-if="errors.location" class="invalid-feedback">{{ errors.location }}</div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="Value">Value [CHF]</label>
-                    <input type="number" class="form-control" v-model="Boat.value" required>
+                    <input
+                        type="number"
+                        class="form-control"
+                        v-model="Boat.value"
+                        @blur="validateField('value')"
+                        :class="{'is-invalid': errors.value}"
+                    >
+                    <div v-if="errors.value" class="invalid-feedback">{{ errors.value }}</div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="description">Further Description</label>
-                    <input type="text" class="form-control" v-model="Boat.description">
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="Boat.description"
+                    >
                 </div>
                 <div class="form-group">
-                    <button class="btn btn-success btn-block">Create</button>
+                    <button
+                        class="btn btn-success btn-block"
+                        :disabled="!isFormValid"
+                    >
+                        Create
+                    </button>
                 </div>
             </form>
         </div>
@@ -43,22 +80,64 @@ export default {
                 location: '',
                 year: '',
                 value: ''
+            },
+            errors: {
+                name: null,
+                year: null,
+                location: null,
+                value: null
             }
         };
     },
+    computed: {
+        isFormValid() {
+            return !Object.values(this.errors).some(error => error !== null);
+        }
+    },
     methods: {
-        handleSubmitForm() {
-            let apiURL = 'https://wxevu5rertmsowcflgfnchf3ua0flgdh.lambda-url.eu-central-1.on.aws/api/';
+        validateField(field) {
+            switch (field) {
+                case 'name':
+                    this.errors.name = this.Boat.name.trim() === '' ? 'Name is required' : null;
+                    break;
+                case 'year':
+                    this.errors.year = /^\d{4}$/.test(this.Boat.year)
+                        ? null
+                        : 'Year must be a 4-digit number';
+                    break;
+                case 'location':
+                    this.errors.location = this.Boat.location.trim() === '' ? 'Location is required' : null;
+                    break;
+                case 'value':
+                    this.errors.value = this.Boat.value > 0
+                        ? null
+                        : 'Value must be a positive number';
+                    break;
+            }
+        },
+        async handleSubmitForm() {
+            for (const field in this.errors) {
+                this.validateField(field);
+            }
 
-            axios.post(apiURL, this.Boat).then(() => {
-                this.$router.push('/view');
-                this.Boat = {
-                    name: '',
-                    description: ''
-                };
-            }).catch(error => {
-                console.log(error);
-            });
+            if (this.isFormValid) {
+                let apiURL = 'https://wxevu5rertmsowcflgfnchf3ua0flgdh.lambda-url.eu-central-1.on.aws/api/';
+                try {
+                    await axios.post(apiURL, this.Boat);
+                    this.$router.push('/view');
+                    this.Boat = {
+                        name: '',
+                        description: '',
+                        location: '',
+                        year: '',
+                        value: ''
+                    };
+                } catch (error) {
+                    console.error('Error creating boat:', error);
+                }
+            } else {
+                console.log('Form has validation errors!');
+            }
         }
     }
 };
--- src/components/EditComponent.vue
+++ src/components/EditComponent.vue
@@ -5,26 +5,63 @@
             <form @submit.prevent="handleUpdateForm">
                 <div class="form-group mb-3">
                     <label for="name">Name</label>
-                    <input type="text" class="form-control" v-model="Boat.name" required>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="Boat.name"
+                        @blur="validateField('name')"
+                        :class="{'is-invalid': errors.name}"
+                    >
+                    <div v-if="errors.name" class="invalid-feedback">{{ errors.name }}</div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="description">Year</label>
-                    <input type="text" class="form-control" v-model="Boat.year" required>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="Boat.year"
+                        @blur="validateField('year')"
+                        :class="{'is-invalid': errors.year}"
+                    >
+                    <div v-if="errors.year" class="invalid-feedback">{{ errors.year }}</div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="location">Location</label>
-                    <input type="text" class="form-control" v-model="Boat.location" required>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="Boat.location"
+                        @blur="validateField('location')"
+                        :class="{'is-invalid': errors.location}"
+                    >
+                    <div v-if="errors.location" class="invalid-feedback">{{ errors.location }}</div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="value">Value [CHF]</label>
-                    <input type="text" class="form-control" v-model="Boat.value" required>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="Boat.value"
+                        @blur="validateField('value')"
+                        :class="{'is-invalid': errors.value}"
+                    >
+                    <div v-if="errors.value" class="invalid-feedback">{{ errors.value }}</div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="description">Additional Information</label>
-                    <input type="text" class="form-control" v-model="Boat.description">
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="Boat.description"
+                    >
                 </div>
                 <div class="form-group">
-                    <button class="btn btn-primary btn-block">Update</button>
+                    <button
+                        class="btn btn-primary btn-block"
+                        :disabled="!isFormValid"
+                    >
+                        Update
+                    </button>
                 </div>
             </form>
         </div>
@@ -43,7 +80,19 @@ export default {
                 location: '',
                 year: '',
                 value: ''
+            },
+            errors: {
+                name: null,
+                year: null,
+                location: null,
+                value: null
             }
+        };
+    },
+    computed: {
+        isFormValid() {
+            // Ensure there are no validation errors
+            return !Object.values(this.errors).some(error => error !== null);
         }
     },
     created() {
@@ -53,14 +102,43 @@ export default {
         });
     },
     methods: {
-        handleUpdateForm() {
-            let apiURL = `https://wxevu5rertmsowcflgfnchf3ua0flgdh.lambda-url.eu-central-1.on.aws/api/${this.$route.params.id}`;
-            axios.put(apiURL, this.Boat).then((res) => {
-                console.log(res);
-                this.$router.push('/view');
-            }).catch(error => {
-                console.log(error);
-            });
+        validateField(field) {
+            switch (field) {
+                case 'name':
+                    this.errors.name = this.Boat.name.trim() === '' ? 'Name is required' : null;
+                    break;
+                case 'year':
+                    this.errors.year = /^\d{4}$/.test(this.Boat.year)
+                        ? null
+                        : 'Year must be a 4-digit number';
+                    break;
+                case 'location':
+                    this.errors.location = this.Boat.location.trim() === '' ? 'Location is required' : null;
+                    break;
+                case 'value':
+                    this.errors.value = this.Boat.value > 0
+                        ? null
+                        : 'Value must be a positive number';
+                    break;
+            }
+        },
+        async handleUpdateForm() {
+            // Validate all fields before submission
+            for (const field in this.errors) {
+                this.validateField(field);
+            }
+
+            if (this.isFormValid) {
+                let apiURL = `https://wxevu5rertmsowcflgfnchf3ua0flgdh.lambda-url.eu-central-1.on.aws/api/${this.$route.params.id}`;
+                try {
+                    await axios.put(apiURL, this.Boat);
+                    this.$router.push('/view');
+                } catch (error) {
+                    console.error('Error updating boat:', error);
+                }
+            } else {
+                console.log('Form has validation errors!');
+            }
         }
     }
 };
