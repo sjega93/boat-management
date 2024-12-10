a91f3a5e1ccde207a53b6f7989fc08d70e1b6a06
Workshop Participant <jegatheeswaran@gmx.ch>
2024-12-10 02:35:31 +0100

    backend validation

--- backend/routes/BouteRoute.js
+++ backend/routes/BouteRoute.js
@@ -1,9 +1,27 @@
 import express from 'express';
 import BoatModel from '../models/Boat.js';
-
+import Joi from 'joi';
 const BoatRoute = express.Router();
 
+//Validate user input for POST and PUT for requestbody 
+const boatSchema = Joi.object({
+    name: Joi.string().trim().required(),
+    description: Joi.string().trim().optional(),
+    year: Joi.string().trim().required(),
+    location: Joi.string().trim().required(),
+    value: Joi.number().required(),
+});
 
+const validateBoat = (req, res, next) => {
+    const { error } = boatSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+        return res.status(400).json({
+            message: 'Validation failed',
+            details: error.details.map(detail => detail.message),
+        });
+    }
+    next();
+};
 // Get Boats
 BoatRoute.route('/').get((req, res, next) =>{
     BoatModel.find((error, data) => {
@@ -16,7 +34,7 @@ BoatRoute.route('/').get((req, res, next) =>{
 })
 
 // Create Boat
-BoatRoute.route('/').post((req, res, next) => {
+BoatRoute.route('/').post(validateBoat,(req, res, next) => {
     BoatModel.create(req.body, (error, data) => {
         if (error) {
             return next(error);
@@ -38,7 +56,7 @@ BoatRoute.route('/:id').get((req, res, next) => {
 })
 
 // Update a specific boat
-BoatRoute.route('/:id').put((req, res, next) => {
+BoatRoute.route('/:id').put(validateBoat,(req, res, next) => {
     BoatModel.findByIdAndUpdate(req.params.id, {
         $set: req.body
     }, (error, data) => {
